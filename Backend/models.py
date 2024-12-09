003db4a28cd5ac05c9d69d16d939851dace424a5
miosmaaland <miosmaaland@gmail.com>
2024-12-09 17:40:53 +0100

    hei sanjai

--- Backend/models.py
+++ Backend/models.py
@@ -1,13 +1,10 @@
-from app import db
-from flask_login import UserMixin
+from flask_sqlalchemy import SQLAlchemy
 
-class User(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False, unique=True)
-    password = db.Column(db.String(200), nullable=False)
+db = SQLAlchemy()
 
-class DataItem(db.Model):
+class Game(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), nullable=False)
-    description = db.Column(db.Text)
-    created_by = db.Column(db.Integer, db.ForeignKey('user.id'))
+    genre = db.Column(db.String(50), nullable=False)
+    release_date = db.Column(db.String(20), nullable=True)
+    status = db.Column(db.String(20), nullable=False, default='Not Completed')
